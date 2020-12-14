b87b06b21b0310185d984fe691a14c6cdd6e7683
Nikolas Ambroziewicz <niko0109@wp.pl>
2020-12-14 22:05:49 +0100

    Finish all logic of JavaScript

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ class Calculator {
     this.firstNumber = "";
     this.secondNumber = "";
     this.operation = undefined;
+    this.afterEqual = "";
   }
 
   addNumber(number) {
@@ -18,27 +19,39 @@ class Calculator {
 
   addOperation(operation) {
     if (this.firstNumber === "") return console.log("cos");
-    if (this.secondNumber !== "") {
+    if (this.secondNumber !== "" && this.afterEqual !== "") {
       this.math();
     }
     this.operation = operation;
     this.secondNumber = this.firstNumber;
     this.firstNumber = "";
-    console.log("tu");
+    console.log(this.operation);
+    if (
+      this.operation === "x^2" ||
+      this.operation === "x^3" ||
+      this.operation === `Sqrt`
+    ) {
+      this.extendsMath();
+    }
   }
 
-  math() {
+  math(text) {
     let score;
     const prev = parseFloat(this.firstNumber);
     console.log(prev);
-    const curent = parseFloat(this.secondNumber);
+    let curent;
+    if (this.secondNumber === "") {
+      curent = parseFloat(this.afterEqual);
+    } else {
+      curent = parseFloat(this.secondNumber);
+    }
     if (isNaN(prev) || isNaN(curent)) return;
     switch (this.operation) {
       case "+":
         score = curent + prev;
         break;
       case "-":
-        score = curent - prev;
+        score = prev - curent;
         break;
       case "X":
         score = curent * prev;
@@ -46,17 +59,60 @@ class Calculator {
       case "/":
         score = curent / prev;
         break;
+      case "%":
+        score = (curent * prev) / 100;
+        break;
     }
+    if (this.afterEqual === "") {
+      this.afterEqual = prev;
+    }
+    this.firstNumber = score;
+    this.secondNumber = "";
+  }
 
-    this.secondNumber = score;
+  extendsMath() {
+    let value;
+    const prev = parseFloat(this.secondNumber);
+    console.log(prev);
+    switch (this.operation) {
+      case "x^2":
+        value = Math.pow(prev, 2);
+        break;
+      case "x^3":
+        value = Math.pow(prev, 3);
+        break;
+      case "Sqrt":
+        value = Math.sqrt(prev);
+        break;
+    }
+    this.firstNumber = value;
+  }
+
+  plusMinus() {
+    if (this.firstNumber.includes("-")) {
+      this.firstNumber = this.firstNumber.substring(1);
+    } else {
+      this.firstNumber = `-${this.firstNumber}`;
+    }
+  }
+
+  clear(text) {
+    if (text === "C") {
+      this.firstNumber = this.firstNumber.slice(0, -1);
+    } else if (text === "CE") {
+      this.firstNumber = "";
+      this.secondNumber = "";
+      this.operation = "";
+      this.afterEqual = 0;
+    }
   }
 
   update() {
-    this.previousDisplay.innerText = `${this.firstNumber} ${
+    this.previousDisplay.innerText = `${this.secondNumber} ${
       this.operation === undefined ? "" : this.operation
     }`;
     this.currentDisplay.innerText = `${
-      this.secondNumber === "" ? this.firstNumber : this.secondNumber
+      this.firstNumber === "" ? this.secondNumber : this.firstNumber
     }`;
   }
 }
@@ -69,6 +125,7 @@ const buttonAllClear = document.querySelector("[data-clearAll]");
 const buttonCleanOne = document.querySelector("[data-clearOne]");
 const buttonProcent = document.querySelector("[data-procent]");
 const buttonEqual = document.querySelector("[data-equal]");
+const buttonPlusMinus = document.querySelector("[data-plusMinus]");
 
 const calculator = new Calculator(previousDispaly, currentDisplay);
 
@@ -87,6 +144,21 @@ buttonOperant.forEach((btn) => {
 });
 
 buttonEqual.addEventListener("click", () => {
-  calculator.math();
+  calculator.math(buttonEqual.innerText);
+  calculator.update();
+});
+
+buttonCleanOne.addEventListener("click", () => {
+  calculator.clear(buttonCleanOne.innerText);
+  calculator.update();
+});
+
+buttonAllClear.addEventListener("click", () => {
+  calculator.clear(buttonAllClear.innerText);
+  calculator.update();
+});
+
+buttonPlusMinus.addEventListener("click", () => {
+  calculator.plusMinus();
   calculator.update();
 });
